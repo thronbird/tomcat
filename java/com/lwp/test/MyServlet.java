20447ed13069f549030486513d6336d7a8f0862d
luli <luli@tsign.cn>
2020-04-04 11:27:36 +0800

    add my own Servlet

---
+++ java/com/lwp/test/MyServlet.java
@@ -0,0 +1,33 @@
+package com.lwp.test;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MyServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("dasdsadsa");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
